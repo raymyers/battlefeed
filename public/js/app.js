bd0514c567bdf6f2720d3d2bd48ecd7c4c496bf0
Ray Myers <ray.myers@gmail.com>
2010-08-09 21:50:13 -0700

    Define String.trim for IE

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,9 @@
+if(typeof String.prototype.trim !== 'function') {
+  String.prototype.trim = function() {
+    return this.replace(/^\s+|\s+$/g, ''); 
+  }
+}
+
 google.load("jquery", "1.4.2");
 google.load("jqueryui", "1.8.3");
 google.setOnLoadCallback(function() {
